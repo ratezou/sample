ad655297e9a3a0c6dd593d0915180f24b9f321f2
ratezou <60992623+ratezou@users.noreply.github.com>
2020-02-13 11:22:01 +0900

    mod result

--- result.php
+++ result.php
@@ -5,7 +5,7 @@ header('Content-Type: text/html; charset=UTF-8');
 // numパラメータにセットする値
 $num = 10;
 // WebAPIのURL
-$url = "http://localhost:1024/api.php?num=${num}";
+$url = "https://ratezou-sample.herokuapp.com/api.php?num=${num}";
 // URLの内容を取得し、json形式からstdClass形式に変換し取得
 $data = json_decode(file_get_contents($url));
 // 連想配列で取得したかったら第二引数にtrueを指定↓
