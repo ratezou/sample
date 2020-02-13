0144a05577d5fb51e4ea932d4a8c423a6f6808b9
ratezou <60992623+ratezou@users.noreply.github.com>
2020-02-13 14:10:39 +0900

    14100213

--- api.php
+++ api.php
@@ -10,37 +10,38 @@ $url = parse_url(getenv('DATABASE_URL'));
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
 
-//DBに登録があればチェックOK
-$stmt = $pdo->prepare("select count(*) cnt from test where key1 = :key1 and key2 = :key2");
-$stmt->execute([
-  ':key1' => $key1,
-  ':key2' => $key2
-]);
-$row = $stmt->fetch();
-if ($row['cnt'] == 1) {
-  result(0, 'already registered');
-}
+try{
+  //DBに登録があればチェックOK
+  $stmt = $pdo->prepare("select count(*) cnt from test where key1 = :key1 and key2 = :key2");
+  $stmt->execute([
+    ':key1' => $key1,
+    ':key2' => $key2
+  ]);
+  $row = $stmt->fetch();
+  if ($row['cnt'] == 1) {
+    result(0, 'already registered');
+  }
 
-//DBに登録がなく、key1の登録件数が2件以上のときはエラー
-$stmt = $pdo->prepare("select count(*) cnt from test where key1 = :key1");
-$stmt->execute([
-':key1' => $key1
-]);
-$row = $stmt->fetch();
-if ($row['cnt'] >= 2) {
-  result(9, 'already exists');
-}
+  //DBに登録がなく、key1の登録件数が2件以上のときはエラー
+  $stmt = $pdo->prepare("select count(*) cnt from test where key1 = :key1");
+  $stmt->execute([
+  ':key1' => $key1
+  ]);
+  $row = $stmt->fetch();
+  if ($row['cnt'] >= 2) {
+    result(9, 'already exists');
+  }
 
-//DBに登録がなく、key1の登録件数が2件未満のときは、DBに登録してチェックOKとする
-$sql = "insert into test(key1, key2) values(:key1, :key2)";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([
-  ':key1' => $key1,
-  ':key2' => $key2
-]);
-  result(0, 'registered');
-} catch (Exception $e){
-  result(9, $e->getMessage());
+  //DBに登録がなく、key1の登録件数が2件未満のときは、DBに登録してチェックOKとする
+  $sql = "insert into test(key1, key2) values(:key1, :key2)";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([
+    ':key1' => $key1,
+    ':key2' => $key2
+  ]);
+    result(0, 'registered');
+  } catch (Exception $e){
+    result(9, $e->getMessage());
 }
 
 
