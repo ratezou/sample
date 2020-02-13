1283fde134b4f69048067bb115f4f67bdc3f4709
ratezou <60992623+ratezou@users.noreply.github.com>
2020-02-13 14:39:03 +0900

    14380213

--- index.php
+++ index.php
@@ -3,25 +3,18 @@
 <head>
   <meta charset="utf-8">
   <title>keys test</title>
-  <style>
-    .key {
-      width: 300px;
-    }
-  </style>
 </head>
 <body>
-  <form action="" id="form">
-    <input type="text" class="key" id="key1" placeholder="enter key1">
-    <input type="text" class="key" id="key2" placeholder="enter key2">
-    <input type="submit" value="post">
+  <form action="" method="post">
+    <input type="text" name="key1">
+    <input type="text" name="key2">
+    <input type="submit" name="submit">
   </form>
   <div id="result"></div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   <script>
     $(function() {
       'use strict';
-
-      $('.key1').focus();
 
       $('form')submit(function(){
         var data = $('form').serializeArray();
