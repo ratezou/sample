08368b3b17fd01413b52e942b8cb95676f9f6e42
ratezou <60992623+ratezou@users.noreply.github.com>
2020-02-13 11:41:43 +0900

    02131141

--- result.php
+++ result.php
@@ -1,18 +1,20 @@
 <?php
-// 文字コード設定
-header('Content-Type: text/html; charset=UTF-8');
-
-// numパラメータにセットする値
-$num = 10;
-// WebAPIのURL
-$url = "https://ratezou-sample.herokuapp.com/api.php?num=${num}";
-// URLの内容を取得し、json形式からstdClass形式に変換し取得
-$data = json_decode(file_get_contents($url));
-// 連想配列で取得したかったら第二引数にtrueを指定↓
-// $data = json_decode(file_get_contents($url), true);
-
-// dataのstatusがyesだったら(出力に成功したら)
-if($data->status == "yes") {
-    // 114倍の値をprint
-    print $data->x114;
-}
+$data = array(
+  “key1” => “value1”
+);
+// JSON形式に変換
+$data = json_encode($data);
+// コンテキストオプションセット
+$options = array(
+  “http” => array(
+    “method” => “POST”,
+    “header” => “Content-type: application/json; charset=UTF-8”,
+    “content” => $data
+  )
+);
+// ストリームコンテキスト生成
+$context = stream_context_create($options);
+// POST送信
+$result = file_get_contents(‘http://localhost/api/test.php’, false, $context);
+// レスポンス表示
+echo $result;
