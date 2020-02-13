e937eeea25ee5fec35a93f98397818bb3be474f7
ratezou <60992623+ratezou@users.noreply.github.com>
2020-02-13 14:27:18 +0900

    14270213

--- index.php
+++ index.php
@@ -1,5 +1,42 @@
-<?php
-$url = parse_url(getenv('DATABASE_URL'));
-$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-$pdo = new PDO($dsn, $url['user'], $url['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>keys test</title>
+  <style>
+    .key {
+      width: 300px;
+    }
+  </style>
+</head>
+<body>
+  <form action="" id="form">
+    <input type="text" class="key" id="key1" placeholder="enter key1">
+    <input type="text" class="key" id="key2" placeholder="enter key1">
+  </form>
+  <div id="result"></div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script>
+    $(function() {
+      'use strict';
+
+      $('#license_key').focus();
+
+      // db操作
+      $('#form').on('submit', function() {
+        // ライセンス（入力値）を取得
+        var key = $('#key').val();
+        // ajax処理
+        $.post('_ajax.php', {
+          key: key,
+          mode: 'activate'
+        }, function(res) {
+          $('#result').text(res.result + ':' + res.message);
+        });
+        return false;
+      });
+
+    });
+  </script>
+</body>
+</html>
