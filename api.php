51ec22dce22716c1fa90b6464ed8c794e1483b1d
ratezou <60992623+ratezou@users.noreply.github.com>
2020-02-13 13:58:26 +0900

    13580213

--- api.php
+++ api.php
@@ -2,5 +2,49 @@
 
 echo "this is api.php\n";
 $json = json_decode(file_get_contents('php://input'), true);
-echo $json['key1'];
-echo $json['key2'];
+$key1 = $json['key1'];
+$key2 = $json['key2'];
+
+//db接続
+$url = parse_url(getenv('DATABASE_URL'));
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+//DBに登録があればチェックOK
+$stmt = $db->prepare("select count(*) cnt from test where key1 = :key1 and key2 = :key2");
+$stmt->execute([
+  ':key1' => $key1,
+  ':key2' => $key2
+]);
+$row = $stmt->fetch();
+if ($row['cnt'] == 1) {
+  result(0, 'already registered');
+}
+
+//DBに登録がなく、key1の登録件数が2件以上のときはエラー
+$stmt = $db->prepare("select count(*) cnt from test where key1 = :key1");
+$stmt->execute([
+':key1' => $key1
+]);
+$row = $stmt->fetch();
+if ($row['cnt'] >= 2) {
+  result(9, 'already exists');
+}
+//DBに登録がなく、key1の登録件数が2件未満のときは、DBに登録してチェックOKとする
+$sql = "insert into test(key1, key2) values(:key1, :key2)";
+$stmt = $db->prepare($sql);
+$stmt->execute([
+  ':key1' => $key1,
+  ':key2' => $key2
+]);
+  result(0, 'registered');
+} catch (Exception $e){
+  result(9, $e->getMessage());
+}
+
+function result($result_code, $result_message) {
+  header("Content-Type: application/json; charset=UTF-8");
+  $result = array('result' => $result_code, 'message' => $result_message);
+  echo json_encode($result);
+  exit;
+}
---
+++ db.sql
@@ -0,0 +1,1 @@
+CREATE TABLE test(key1 varchar(32), key2 varchar(32), PRIMARY KEY (key1, key2));
