59ce3a577385eabb2b5669a1f55c6e734da2da5a
ratezou <60992623+ratezou@users.noreply.github.com>
2020-02-13 14:34:33 +0900

    14340213

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
 <body>
   <form action="" id="form">
     <input type="text" class="key" id="key1" placeholder="enter key1">
-    <input type="text" class="key" id="key2" placeholder="enter key1">
+    <input type="text" class="key" id="key2" placeholder="enter key2">
+    <input type="submit" value="post">
   </form>
   <div id="result"></div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
@@ -20,21 +21,12 @@
     $(function() {
       'use strict';
 
-      $('#license_key').focus();
+      $('.key1').focus();
 
-      // db操作
-      $('#form').on('submit', function() {
-        // ライセンス（入力値）を取得
-        var key = $('#key').val();
-        // ajax処理
-        $.post('_ajax.php', {
-          key: key,
-          mode: 'activate'
-        }, function(res) {
-          $('#result').text(res.result + ':' + res.message);
-        });
-        return false;
-      });
+      $('form')submit(function(){
+        var data = $('form').serializeArray();
+        console.log(data);
+      }
 
     });
   </script>
