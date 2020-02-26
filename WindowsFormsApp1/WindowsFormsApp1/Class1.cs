5f3d4b3dae405e4523b7de5db29a934e3fa5a05d
ratezou <60992623+ratezou@users.noreply.github.com>
2020-02-26 15:17:20 +0900

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    [JsonObject]
+    public class request
+    {
+        [JsonProperty("key1")]
+        public string key1 { get; set; }
+        [JsonProperty("key2")]
+        public string key2 { get; set; }
+    };
+    public class response
+    {
+        [JsonProperty("result")]
+        public string result { get; set; }
+        [JsonProperty("message")]
+        public string message { get; set; }
+    };
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            POST();
+        }
+
+        private async void POST()
+        {
+            var client = new HttpClient();
+            var json = JsonConvert.SerializeObject(new request() { key1 = textBox1.Text, key2 = textBox2.Text });
+            Encoding utf8 = Encoding.UTF8;
+            var content = new StringContent(json, utf8, "application/json");
+            var responce = await client.PostAsync("https://ratezou-sample.herokuapp.com/api.php", content);
+
+            var resString = await responce.Content.ReadAsStringAsync();
+            var resJson = JsonConvert.DeserializeObject<response>(resString);
+
+            label1.Text = resJson.result + ":" + resJson.message;
+        }
+    }
+}
